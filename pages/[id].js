d54d6b2d3530131356c6a51eaba477c23233efc4
yogangr <ynugroho487@gmail.com>
2023-06-03 13:52:20 +0700

    update-char-and-trailer

--- pages/[id].js
+++ pages/[id].js
@@ -2,17 +2,43 @@ import Layout from "@/component/Layout";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
+import ReactPlayer from "react-player";
+import Card from "react-bootstrap/Card";
+import Link from "next/link";
 
-function Detail({ anime }) {
+function Detail({ anime, char }) {
   const renderGenre = () => {
     return anime.genres.map((type) => (
       <span key={type.mal_id}> {type.name},</span>
     ));
   };
+
+  const renderTrailer = () => {
+    if (anime.trailer.url) {
+      return (
+        <>
+          <div className="my-5">
+            <h2 className="text-center fw-bold">Trailer {anime.title}</h2>
+            <div className="d-flex justify-content-center">
+              <ReactPlayer
+                width="100vw"
+                height="100vh"
+                url={anime.trailer.url}
+              />
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      <>
+        <div></div>
+      </>;
+    }
+  };
   return (
     <Layout>
       <div className="anime-detail my-4">
-        <Container>
+        <Container className=" justify-content-center ">
           <Row>
             <Col className="col-4 thumb">
               <img
@@ -35,6 +61,33 @@ function Detail({ anime }) {
               <p>Genre : {renderGenre()}</p>
             </Col>
           </Row>
+          <Row className="list-char gap-3 my-5">
+            <div className="fw-bold h1">Karakter</div>
+            {char.map((chara) => (
+              <Card
+                style={{ width: "115px", height: "14rem" }}
+                key={chara.character.mal_id}
+              >
+                <Link href={`${chara.character.mal_id}/character`}>
+                  <Card.Img
+                    variant="top"
+                    src={chara.character.images.jpg.image_url}
+                    className="char-img"
+                  />
+                </Link>
+                <Card.Body>
+                  <Card.Title className="title-char">
+                    {chara.character.name}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            ))}
+          </Row>
+
+          {/* {anime.map((post, index) => (
+            <div key={index}>Characters : {post.character.name}</div>
+          ))} */}
+          {renderTrailer()}
         </Container>
       </div>
     </Layout>
@@ -49,9 +102,15 @@ export async function getServerSideProps(context) {
   );
   const { data } = await res.json();
   const anime = data;
+  const response = await fetch(
+    `https://api.jikan.moe/v4/anime/${context.params.id}/characters`
+  );
+  const result = await response.json();
+  const char = result.data;
   return {
     props: {
       anime,
+      char,
     },
   };
 }
